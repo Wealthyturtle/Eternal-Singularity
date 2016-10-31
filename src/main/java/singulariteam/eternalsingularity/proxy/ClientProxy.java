1726d33cce6f7adc1e129d2de05551c30bc97aad
RCXcrafter <rcxcrafter@gmail.com>
2016-10-31 20:56:26 +0100

    I think this is good?
    What am I doing?

--- src/main/java/singulariteam/eternalsingularity/proxy/ClientProxy.java
+++ src/main/java/singulariteam/eternalsingularity/proxy/ClientProxy.java
@@ -9,13 +9,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
 import singulariteam.eternalsingularity.EternalSingularityItem;
+import singulariteam.eternalsingularity.render.EternalItemRenderer;
 
 public final class ClientProxy extends CommonProxy
 {
 	@Override
 	public void init()
 	{
-		MinecraftForgeClient.registerItemRenderer(eternalSingularity, new CosmicItemRenderer());
-		MinecraftForgeClient.registerItemRenderer(eternalSingularity, new FancyHaloRenderer());
+		MinecraftForgeClient.registerItemRenderer(eternalSingularity, new EternalItemRenderer());
 	}
 }
