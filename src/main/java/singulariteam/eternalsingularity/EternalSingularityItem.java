2a20460d3e952c5d71ba5d47de054758bb02f85d
RCXcrafter <rcxcrafter@gmail.com>
2016-10-31 19:33:05 +0100

    Remove some files
    Also more or less added the eternal singularity.

--- src/main/java/singulariteam/eternalsingularity/EternalSingularityItem.java
+++ src/main/java/singulariteam/eternalsingularity/EternalSingularityItem.java
@@ -2,10 +2,13 @@ package singulariteam.eternalsingularity;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import fox.spiteful.avaritia.Avaritia;
 import fox.spiteful.avaritia.entity.EntityImmortalItem;
+import fox.spiteful.avaritia.items.ItemResource;
 import fox.spiteful.avaritia.items.ItemSingularity;
 import fox.spiteful.avaritia.items.LudicrousItems;
 import fox.spiteful.avaritia.render.ICosmicRenderItem;
+import fox.spiteful.avaritia.render.IHaloRenderItem;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
@@ -18,25 +21,17 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
-public class EternalSingularityItem extends ItemSingularity implements ICosmicRenderItem
+public class EternalSingularityItem extends Item implements IHaloRenderItem, ICosmicRenderItem
 {
 	private IIcon cosmicMask;
+	private IIcon foregroundIcon;
 
 	public EternalSingularityItem()
 	{
 		super();
-		setHasSubtypes(false);
-	}
-
-	@SideOnly(Side.CLIENT)
-	public int getColorFromItemStack(ItemStack itemstack, int renderpass)
-	{
-		return 0xFFFFFF;
-	}
-
-	public void setDamage(ItemStack stack, int damage)
-	{
-		super.setDamage(stack, 0);
+		setCreativeTab(Avaritia.tab);
+		setTextureName("eternalsingularity:eternal_singularity2");
+		setUnlocalizedName(Reference.MOD_ID + ".eternal.singularity");
 	}
 
 	public EnumRarity getRarity(ItemStack stack)
@@ -44,35 +39,10 @@ public class EternalSingularityItem extends ItemSingularity implements ICosmicRe
 		return LudicrousItems.cosmic;
 	}
 
-	@SideOnly(Side.CLIENT)
-	public IIcon getMaskTexture(ItemStack stack, EntityPlayer player)
-	{
-		return this.cosmicMask;
-	}
-
-	@SideOnly(Side.CLIENT)
-	public float getMaskMultiplier(ItemStack stack, EntityPlayer player)
-	{
-		return 1.0F;
-	}
-
 	@Override
 	public String getUnlocalizedName(final ItemStack stack)
 	{
-		return "item.eternalSingularity";
-	}
-
-	@SideOnly(Side.CLIENT)
-	public void registerIcons(IIconRegister ir)
-	{
-		super.registerIcons(ir);
-		this.cosmicMask = ir.registerIcon("universalsingularities:eternalSingularity_cosmicMask2");
-	}
-
-	@SideOnly(Side.CLIENT)
-	public boolean requiresMultipleRenderPasses()
-	{
-		return true;
+		return "item.eternal_singularity";
 	}
 
 	@Override
@@ -81,12 +51,69 @@ public class EternalSingularityItem extends ItemSingularity implements ICosmicRe
 		return true;
 	}
 
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getMaskTexture(ItemStack stack, EntityPlayer player) {
+        return cosmicMask;
+    }
+    
+    @Override
+	@SideOnly(Side.CLIENT)
+	public float getMaskMultiplier(ItemStack stack, EntityPlayer player) {
+		return 1.0f;
+	}
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IIconRegister ir) {
+        super.registerIcons(ir);
+
+        this.cosmicMask = ir.registerIcon("eternalsingularity:eternal_singularity_mask");
+        this.foregroundIcon = ir.registerIcon("eternalsingularity:eternal_singularity");
+    }
+
+    @Override
+    public IIcon getIcon(ItemStack stack, int pass){
+    	if (pass == 1) { return this.foregroundIcon; }
+
+    	return super.getIcon(stack, pass);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean requiresMultipleRenderPasses()
+    {
+        return true;
+}
+
 	@Override
 	public Entity createEntity(World world, Entity location, ItemStack itemstack)
 	{
 		return new EntityImmortalItem(world, location, itemstack);
 	}
 
-	@Override
-	public void getSubItems(final Item item, final CreativeTabs tab, final List list) {}
+	@SideOnly(Side.CLIENT)
+	public boolean drawHalo(ItemStack paramItemStack) {
+		return true;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public IIcon getHaloTexture(ItemStack paramItemStack) {
+		return ((ItemResource)LudicrousItems.resource).halo[0];
+	}
+
+	@SideOnly(Side.CLIENT)
+	public int getHaloSize(ItemStack paramItemStack) {
+		return 30;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public boolean drawPulseEffect(ItemStack paramItemStack) {
+		return true;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public int getHaloColour(ItemStack paramItemStack) {
+		return 0xFF20DB9A;
+	}
 }
--- src/main/java/singulariteam/eternalsingularity/proxy/ClientProxy.java
+++ src/main/java/singulariteam/eternalsingularity/proxy/ClientProxy.java
@@ -3,6 +3,7 @@ package singulariteam.eternalsingularity.proxy;
 import org.lwjgl.opengl.GL11;
 
 import fox.spiteful.avaritia.items.LudicrousItems;
+import fox.spiteful.avaritia.render.CosmicItemRenderer;
 import fox.spiteful.avaritia.render.FancyHaloRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
@@ -14,8 +15,7 @@ public final class ClientProxy extends CommonProxy
 	@Override
 	public void init()
 	{
-		final FancyHaloRenderer fancyHaloRenderer = new FancyHaloRenderer();
-		singularities.forEach(singularity -> MinecraftForgeClient.registerItemRenderer(singularity, fancyHaloRenderer));
-		MinecraftForgeClient.registerItemRenderer(eternalSingularity, fancyHaloRenderer);
+		MinecraftForgeClient.registerItemRenderer(eternalSingularity, new CosmicItemRenderer());
+		MinecraftForgeClient.registerItemRenderer(eternalSingularity, new FancyHaloRenderer());
 	}
 }
