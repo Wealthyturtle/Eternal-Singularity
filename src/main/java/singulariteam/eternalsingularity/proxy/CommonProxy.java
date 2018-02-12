4e82ba8d5f58cc8629b201e2ef65f883c8c33d0c
RCXcrafter <rcxcrafter@gmail.com>
2018-02-12 19:10:10 +0100

    that's the wrong number

--- src/main/java/singulariteam/eternalsingularity/proxy/CommonProxy.java
+++ src/main/java/singulariteam/eternalsingularity/proxy/CommonProxy.java
@@ -82,7 +82,7 @@ public class CommonProxy {
 			compoundSingularityItem.max = compoundMax;
 			final List<Object> eternalSingularityRecipeInputs = eternalSingularityRecipe.getInput();
 			for (int i = 0; i < compoundMax; i++) {
-				final ShapelessOreRecipe compoundRecipe = new ShapelessOreRecipe(new ItemStack(compoundSingularityItem, 1, MathHelper.clamp_int(i, 1, 64)));
+				final ShapelessOreRecipe compoundRecipe = new ShapelessOreRecipe(new ItemStack(compoundSingularityItem, 1, MathHelper.clamp_int(i, 0, 64)));
 				for (int s = 0; s < 9; s++) {
 					final int pos = 9 * i + s;
 					if (pos > singularityCount - 1)
