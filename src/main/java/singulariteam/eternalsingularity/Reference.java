767880e53e2433e97e54e25fc182d0b85dd4df06
WanionCane <wanioncane@gmail.com>
2018-06-30 13:04:19 -0300

    bump version...

--- src/main/java/singulariteam/eternalsingularity/Reference.java
+++ src/main/java/singulariteam/eternalsingularity/Reference.java
@@ -4,7 +4,7 @@ public final class Reference
 {
 	public static final String MOD_ID = "eternalsingularity";
 	public static final String MOD_NAME = "Eternal Singularity";
-	public static final String MOD_VERSION = "1.7.10-.1.0.3b";
+	public static final String MOD_VERSION = "1.7.10-.1.0.4b";
 	public static final String MC_VERSION = "[1.7.10]";
 	static final String COMMON_PROXY = "singulariteam.eternalsingularity.proxy.CommonProxy";
 	static final String CLIENT_PROXY = "singulariteam.eternalsingularity.proxy.ClientProxy";
